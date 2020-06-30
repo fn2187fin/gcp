cd6aa09462e817d046e40c5206b83c87717ee842
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-06-30 17:02:50 +0900

    revise

--- bin/2ja.sh
+++ bin/2ja.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 if [ $# -lt 1 ];then
-    exit
+    word=$(echo "$(cat)" | cut -b 1-40 | nkf -WwMQ |tr = %)
+else
+    word=$(echo $* | cut -b 1-40 | nkf -WwMQ |tr = %)
 fi
 
 lang4=en
 lang2=ja
 
-word=$*
-#echo $word
-word=$(echo $* | cut -b 1-40 | nkf -WwMQ |tr = %)
-#word=$(echo $* | nkf -LuWwMQ | tr = %)
-#word=$(echo $word | sed -e 's/ /%20/g')
-#word=$(echo $word | sed -e 's/ /%20/g' -e 's/$/%22/')
-#echo $word
 
 req=$(echo 'https://script.google.com/macros/s/AKfycbwTMyNXe0JroZEy0UA6DHx34-FeH-s48oqYRbNxofxGh8amqkp-/exec?')
 req=$(echo ${req}text=${word}?source=${lang4}?target=${lang2})
